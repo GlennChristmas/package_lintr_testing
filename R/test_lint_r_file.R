a7154148df390fc0216ff76c3f7c8dcc193f748a
Glenn Christmas <58911316+GlennChristmas@users.noreply.github.com>
2023-08-18 21:40:50 +0100

    Delete test_lint_r_file.R

--- R/test_lint_r_file.R
+++
@@ -1,5 +0,0 @@
-#lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots
-
-library(tibble)
-
-trib1 <- tibble::tribble(~"item1",~"item2",1,2,1,2,1,2,1,2,1,2)
