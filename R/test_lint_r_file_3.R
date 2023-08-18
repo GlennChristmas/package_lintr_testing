1fa59a1ff173d11e2e4b3fa418d57527f38c42d1
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2023-08-18 22:07:40 +0100

    fix roxygen dependency issues

--- R/test_lint_r_file_3.R
+++ R/test_lint_r_file_3.R
@@ -1,5 +1,8 @@
 #lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots lots and lots and lots
 
-trib1 <- tibble::tribble(~"item1",~"item2",1,2,1,2,1,2,1,2,1,2)
-
+#' @import tibble
+func1 <- function(x){
+  trib1 <- tibble::tribble(~"item1",~"item2",x,2,1,2,1,2,1,2,1,2)
+  return(trib1)
+}
 #1
